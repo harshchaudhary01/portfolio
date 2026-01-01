81d266084b8f62b520780b26d9d6fb6f8df181ff
harshchaudhary01 <harshcareer01@gmail.com>
2026-01-01 15:21:48 +0530

    Configuring Tailwind Css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-5xl font-bold text-white min-h-screen flex items-center justify-center bg-black'>
+      Hello Portfolio
+    </div>
+  )
+}
+
+export default App
